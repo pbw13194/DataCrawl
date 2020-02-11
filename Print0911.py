0d0db71f633ee11948342cf70f9616d4089b9392
pbw13194 <60160724+pbw13194@users.noreply.github.com>
2020-02-11 15:17:24 -0500

    Add files via upload

---
+++ Print0911.py
@@ -0,0 +1,23 @@
+# This is our first Python program
+
+print('Programming with Python is fun!')
+print('My name is Xu Han')
+print("Programming with Python is fun!")
+print("My name is Xu Han")
+print("Don't be afraid")
+print("I'm here")
+print('Your assignment "Homework 1" is due tomorrow')
+print('''Xu said:"Dont't be afraid of programming"''')   # What Xu said is right!
+
+print(2)
+print(2+3)
+a=2
+print(a)
+print(a+2)
+print('a')
+print("''")
+print('""')
+print('#')
+print("'")
+print('\'')
+print('\"')
